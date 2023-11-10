90ddc73960c84bc6795b36c48c0b3d46b7cf3849
QueSchoenenberger <schoenenbergerq@bzz.ch>
2023-11-10 16:46:32 +0100

    did everything

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, send_file
+import subprocess
 
 app = Flask(__name__)
 
@@ -6,6 +7,115 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/A1G')
+def a1g_page():
+    return render_template('pages/A/A1G.html')
+
+
+@app.route('/A1F')
+def a1f_page():
+    return render_template('pages/A/A1F.html')
+
+
+@app.route('/A1E')
+def a1e_page():
+    return render_template('pages/A/A1E.html')
+
+
+@app.route('/B1G')
+def b1g_page():
+    return render_template('pages/B/B1G.html')
+
+
+@app.route('/B1F')
+def b1f_page():
+    return render_template('pages/B/B1F.html')
+
+
+@app.route('/B1E')
+def b1e_page():
+    return render_template('pages/B/B1E.html')
+
+
+@app.route('/B2G')
+def b2g_page():
+    return render_template('pages/B/B2G.html')
+
+
+@app.route('/B2F')
+def b2f_page():
+    return render_template('pages/B/B2F.html')
+
+
+@app.route('/B2E')
+def b2e_page():
+    return render_template('pages/B/B2E.html')
+
+
+@app.route('/B3G')
+def b3g_page():
+    return render_template('pages/B/B3G.html')
+
+
+@app.route('/B3F')
+def b3f_page():
+    return render_template('pages/B/B3F.html')
+
+
+@app.route('/B3E')
+def b3e_page():
+    return render_template('pages/B/B3E.html')
+
+
+@app.route('/B4G')
+def b4g_page():
+    return render_template('pages/B/B4G.html')
+
+
+@app.route('/B4F')
+def b4f_page():
+    return render_template('pages/B/B4F.html')
+
+
+@app.route('/B4E')
+def b4e_page():
+    return render_template('pages/B/B4E.html')
+
+
+@app.route('/C1G')
+def c1g_page():
+    return render_template('pages/C/C1G.html')
+
+
+@app.route('/C1F')
+def c1f_page():
+    return render_template('pages/C/C1F.html')
+
+
+@app.route('/C1E')
+def c1e_page():
+    return render_template('pages/C/C1E.html')
+
+
+@app.route('/runcode', methods=['POST'])
+def run_code():
+    try:
+        code = request.json['code']
+        result = subprocess.check_output(['python', '-c', code], stderr=subprocess.STDOUT, text=True)
+        return jsonify({'output': result})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+@app.route('/download_pdf/<filename>')
+def download_pdf(filename):
+    # Specify the path to the PDF files directory
+    pdfs_directory = 'static/doc_files/'
+
+    # Use Flask's send_file function to send the requested file for download
+    return send_file(pdfs_directory + filename, as_attachment=True)
 
 
 if __name__ == '__main__':
--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let tableRows = document.querySelectorAll(".clickable-row");
-    tableRows.forEach(function(row) {
-        let cells = row.getElementsByTagName("td");
-        for (let i = 0; i < cells.length; i++) {
-            cells[i].addEventListener("click", function() {
-                let url = row.getAttribute("data-href");
-                if (url) {
-                    window.location.href = url;
-                }
-            });
+
+
+// Get all elements with the "clickable-cell" class by class name
+let clickableCells = document.getElementsByClassName('clickable-cell');
+
+// Add click event listeners to the clickable cells
+for (let i = 0; i < clickableCells.length; i++) {
+    clickableCells[i].addEventListener('click', function() {
+        let dataHref = this.getAttribute('data-href');
+        console.log(dataHref)
+        if (dataHref) {
+            window.location.href = dataHref; // Navigate to the specified URL
         }
     });
-});
+}
+
+
+function runCode(elementId) {
+    // Get the code and output elements by their IDs
+    const codeElement = document.getElementById(elementId);
+    const outputElement = codeElement;
+
+    // Get the code from the <pre> element
+    const code = codeElement.textContent;
+
+    // Send the code to the backend using a POST request
+    fetch('/runcode', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ code: code }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Display the output in the <pre> element with ">>"
+        if (data.error) {
+            outputElement.innerText += '\n>> Error: ' + data.error;
+        } else {
+            outputElement.innerText += '\n>> Output:\n' + data.output;
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+function download(filename) {
+        // Construct the download URL with the filename parameter
+        window.location.href = '/download_pdf/' + filename;
+    }
